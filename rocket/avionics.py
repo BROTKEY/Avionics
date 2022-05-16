ca8776685ee31542a1b673b2a4e71f8f4866a90f
Jakob Lehnert <jakob.lehnert@xavo.com>
2022-05-16 22:01:42 +0200

    update firmware

--- rocket/avionics.py
+++ rocket/avionics.py
@@ -9,9 +9,12 @@ import RPi.GPIO as GPIO
 import time
 
 RUN = True
+TARGETIP = "192.168.43.59"
 
 relaisOne = 17
 relaiTwo = 27
+i2c = board.I2C()
+bno = adafruit_bno055.BNO055_I2C(i2c)
 
 
 def BMP388():
@@ -23,23 +26,32 @@ def BMP388():
         while RUN:
             data = "{0:0>9.4f},{1:0>9.4f},{2:0>8.4f}".format(
                 bmp.altitude, bmp.pressure, bmp.temperature)
-            sock.sendto(bytes(data, "utf-8"), ("192.168.43.59", 691))
+            sock.sendto(bytes(data, "utf-8"), (TARGETIP, 691))
             myfile.write(data+"\n")
 
 
 def BNO055Euler():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    i2c = board.I2C()
-    bno = adafruit_bno055.BNO055_I2C(i2c)
-    with open("BNO055_Logs.csv", "w") as myfile:
+    with open("BNO055Euler_Logs.csv", "w") as myfile:
         myfile.write("x,y,z\n")
         while RUN:
             x, y, z = bno.euler
             if type(x) == float and type(y) == float and type(y) == float:
                 if (x < 360 and y < 360 and z < 360) and (x > -360 and y > -360 and z > -360):
                     data = "{0:0<9.4f},{1:0<9.4f},{2:0<8.4f}".format(x, y, z)
-                    sock.sendto(bytes(data, "utf-8"), ("192.168.43.59", 692))
+                    sock.sendto(bytes(data, "utf-8"), (TARGETIP, 692))
                     myfile.write(data+"\n")
+
+def BNO055Accel():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    with open("BNO055Accel_Logs.csv", "w") as myfile:
+        myfile.write("x,y,z\n")
+        while RUN:
+            x, y, z = bno.acceleration
+            if type(x) == float and type(y) == float and type(y) == float:
+                data = "{0:0<9.4f},{1:0<9.4f},{2:0<8.4f}".format(x, y, -z)
+                sock.sendto(bytes(data, "utf-8"), (TARGETIP, 693))
+                myfile.write(data+"\n")                    
 
 
 def NoLoopGuidance():
@@ -77,21 +89,25 @@ def camera():
 
 BMP388Thread = threading.Thread(target=BMP388)
 BMP388Thread.start()
-BNO055Thread = threading.Thread(target=BNO055Euler)
-BNO055Thread.start()
+BNO055ThreadGyro = threading.Thread(target=BNO055Euler)
+BNO055ThreadGyro.start()
+BNO055ThreadAccel = threading.Thread(target=BNO055Accel)
+BNO055ThreadAccel.start()
 LaunchThread = threading.Thread(target=NoLoopGuidance)
 LaunchThread.start()
-#cameraThread = threading.Thread(target=camera)
-# cameraThread.start()
+cameraThread = threading.Thread(target=camera)
+cameraThread.start()
 
 print("Startup succesfull")
 input()
 RUN = False
 BMP388Thread.join()
 print("Stopped BMP388")
-BNO055Thread.join()
-print("Stopped BNO055")
-# cameraThread.join()
+BNO055ThreadGyro.join()
+print("Stopped BNO55Gyro")
+BNO055ThreadAccel.join()
+print("Stopped BNO055Accel")
+cameraThread.join()
 print("Stopped Camera")
 LaunchThread.join()
 print("Stopped Launch")
